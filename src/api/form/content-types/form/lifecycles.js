dd29e457f2a0d12e422058e606618f58914ee11d
Pham Minh Tuan <phmtuan313@gmail.com>
2023-11-20 21:32:44 +0700

    testing send email

--- src/api/form/content-types/form/lifecycles.js
+++ src/api/form/content-types/form/lifecycles.js
@@ -1,7 +1,13 @@
 module.exports = {
 
-    afterCreate(event) {
+    async afterCreate(event) {
         const { result, params } = event;
-        console.log('afterCreate', result, params)
+
+        await strapi.service("api::form.email").send(
+            "hsrlab2023",
+            "phmtuan313@gmail.com",
+            "Welcome",
+            "Testing"
+        );
     }
 }
