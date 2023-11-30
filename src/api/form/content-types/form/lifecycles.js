7400c7bbdecf886f76df9cb658cbb8a5ca5f7d00
Pham Minh Tuan <phmtuan313@gmail.com>
2023-11-30 14:14:11 +0700

    remove life cycle

--- src/api/form/content-types/form/lifecycles.js
+++ src/api/form/content-types/form/lifecycles.js
@@ -1,13 +1,1 @@
-module.exports = {
-
-    async afterCreate(event) {
-        const { result, params } = event;
-
-        await strapi.service("api::form.email").send(
-            "hsrlab2023",
-            "phmtuan313@gmail.com",
-            "Welcome",
-            "Testing"
-        );
-    }
-}
+module.exports = {}
